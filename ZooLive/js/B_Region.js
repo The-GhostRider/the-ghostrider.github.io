d40c75401d78cce81fef9a0d00ff652f14544020
The GhostRider <xspidey@hotmail.it>
2023-11-06 17:42:49 +0000

    Region-CH1.1

--- ZooLive/js/B_Region.js
+++ ZooLive/js/B_Region.js
@@ -13,22 +13,29 @@ function isIPExcluded(ipToCheck, exclusionList) {
     return exclusionList.includes(ipToCheck);
 }
 
+// Funzione per verificare se una città è inclusa nella lista di reindirizzamento
+function isCityIncluded(cityToCheck, inclusionList) {
+    return inclusionList.includes(cityToCheck);
+}
+
 // Funzione per reindirizzare una regione specifica
-function redirectRegion(regionToRedirect, redirectURL, ipExclusionList) {
+function redirectRegion(regionToRedirect, redirectURL, cityInclusionList, ipExclusionList) {
     $.get("https://ipinfo.io", function(response) {
         const location = response.country;
+        const city = response.city;
         const ip = response.ip;
 
-        if (location === regionToRedirect && !isIPExcluded(ip, ipExclusionList)) {
+        if (location === regionToRedirect && isCityIncluded(city, cityInclusionList) && !isIPExcluded(ip, ipExclusionList)) {
             // Reindirizza alla pagina specificata
             window.location.href = redirectURL;
         }
     }, "jsonp");
 }
 
-// Reindirizza la regione "CH" (Svizzera) a una pagina diversa e permette l'accesso solo agli IP esclusi
+// Reindirizza le città Selezionate della Regione a una pagina diversa
 const regionToRedirect = "CH";
 const redirectURL = "ch.html"; // URL della pagina di reindirizzamento
-const ipExclusionList = ["192.168.0.1", "10.0.0.2"]; // Esempio di IP da escludere
+const cityInclusionList = ["Lausanne", "Geneva"]; // Esempio di città da includere
+const ipExclusionList = ["0.0.0.0", "0.0.0.0"]; // Esempio di IP da escludere
 
-redirectRegion(regionToRedirect, redirectURL, ipExclusionList);
+redirectRegion(regionToRedirect, redirectURL, cityInclusionList, ipExclusionList);
